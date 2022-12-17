b2ea58e1d62b63f574baf4b780a4f7fe79f298cb
Oleksandr <dacenko@gmail.com>
2022-12-17 23:31:54 +0200

    Updated module rectangle.py and main.py

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
-import rectangle
+from rectangle import Rectangle
 
 
 if __name__ == "__main__":
-    pass
+    try:
+        rect_1 = Rectangle(1, 2)
+        rect_2 = Rectangle(2, 3)
+        rects_sum = rect_1 + rect_2
+        rects_mul_num = rect_2 * 2
+        rects_mul = rect_1 * rect_2
+    except TypeError:
+        pass
+
+    print(rects_sum)
+    print(rects_mul_num)
+
+
 
--- rectangle.py
+++ rectangle.py
@@ -1,4 +1,4 @@
-"""This module provides access to class `Rectangle`."""
+"""This module provides access to class Rectangle."""
 
 class Rectangle:
     """
@@ -11,21 +11,60 @@ class Rectangle:
     Attributes:
         a (int): The length of the rectangle.
         b (int): The width of the rectangle.
-
-    Methods:
-        square(a, b):
-            Returns the square of the rectangle.
-
     """
 
     def __init__(self, a: int, b: int):
-
-
         self.a = a
         self.b = b
 
-    def __str__(self):
+    def square (self) -> int:
+        """Returns the square of the rectangle."""
 
+        return self.a * self.b
+
+    def __gt__(self, other):
+        return self.square() > other.square()
+
+    def __lt__(self, other):
+        return self.square() < other.square()
+
+    def __ge__(self, other):
+        return self.square() >= other.square()
+
+    def __le__(self, other):
+        return self.square() <= other.square()
+
+    def __add__(self, other):
+        """Return self+other"""
+
+        if isinstance(other, Rectangle):
+            return self.square() + other.square()
+        if isinstance(other, int):
+            return self.square() + other
+        return NotImplemented
+
+    def __radd__(self, other):
+        """Return other+self"""
+
+        if isinstance(other, int):
+            return self.square() + other
+        return NotImplemented
+
+    def __mul__(self, other):
+        """Return self*other"""
+
+        if isinstance(other, int | float):
+            return self.square() * other
+        return NotImplemented
+
+    def __rmul__(self, other):
+        """Return other*self"""
+
+        if isinstance(other, int) or isinstance(other, float):
+            return self.square() * other
+        return NotImplemented
+
+    def __str__(self):
         return f"{self.a} x {self.b}"
 
 
